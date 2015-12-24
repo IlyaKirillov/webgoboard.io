3d5107aaa68cda4eb2e2010fed4bbcc31281b451
IlyaKirillov <kocmohabt.baduk@gmail.com>
2015-12-24 20:50:05 +0300

    Ссылки с goban.org заменены на webgoboard.com

--- javascripts/main.js
+++ javascripts/main.js
@@ -28,12 +28,12 @@ function LoadMenu()
         oMenu.appendChild(oLi);
     }
 
-    AddMenuItem(oMenu, "http://www.goban.org/index", "Kogo's joseki");
-    AddMenuItem(oMenu, "http://www.goban.org/19", "Board 19x19");
-    AddMenuItem(oMenu, "http://www.goban.org/13", "Board 13x13");
-    AddMenuItem(oMenu, "http://www.goban.org/9", "Board 9x9");
-    AddMenuItem(oMenu, "http://www.goban.org/introduction", "Introduction to Go");
-    AddMenuItem(oMenu, "http://www.goban.org/problems", "Problems");
+    AddMenuItem(oMenu, "http://www.webgoboard.org/index", "Kogo's joseki");
+    AddMenuItem(oMenu, "http://www.webgoboard.org/19", "Board 19x19");
+    AddMenuItem(oMenu, "http://www.webgoboard.org/13", "Board 13x13");
+    AddMenuItem(oMenu, "http://www.webgoboard.org/9", "Board 9x9");
+    AddMenuItem(oMenu, "http://www.webgoboard.org/introduction", "Introduction to Go");
+    AddMenuItem(oMenu, "http://www.webgoboard.org/problems", "Problems");
 
     document.body.appendChild(oMenu);
 }
@@ -56,7 +56,7 @@ function LoadMainDiv()
 function LoadGameTree()
 {
     oGameTree = GoBoardApi.Create_GameTree();
-    GoBoardApi.Set_Sound(oGameTree, 'http://goban.org/Sound');
+    GoBoardApi.Set_Sound(oGameTree, 'http://webgoboard.org/Sound');
     GoBoardApi.Create_BoardCommentsButtonsNavigator(oGameTree, "divId");
     GoBoardApi.Focus(oGameTree);
     window.onresize();
@@ -65,7 +65,7 @@ function LoadGameTree()
 function LoadGameTreePresentation(aSlides)
 {
     oGameTree = GoBoardApi.Create_GameTree();
-    GoBoardApi.Set_Sound(oGameTree, 'http://goban.org/Sound');
+    GoBoardApi.Set_Sound(oGameTree, 'http://webgoboard.org/Sound');
     GoBoardApi.Create_Presentation(oGameTree, "divId", aSlides);
     GoBoardApi.Focus(oGameTree);
     window.onresize();
@@ -74,7 +74,7 @@ function LoadGameTreePresentation(aSlides)
 function LoadGameTreeProblems()
 {
     oGameTree = GoBoardApi.Create_GameTree();
-    GoBoardApi.Set_Sound(oGameTree, 'http://goban.org/Sound');
+    GoBoardApi.Set_Sound(oGameTree, 'http://webgoboard.org/Sound');
     GoBoardApi.Create_Problems(oGameTree, "divId", {TutorColor : "Auto", TutorTime : 0.2, NewNode : ""});
     GoBoardApi.Focus(oGameTree);
     window.onresize();
